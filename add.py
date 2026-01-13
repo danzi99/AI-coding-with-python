89c7d1a444ee400a9261df1e0958d4f9e474b227
danzi99 <nanjoo@gmail.com>
2026-01-13 18:53:21 +0000

    Add files via upload

---
+++ add.py
@@ -0,0 +1,6 @@
+import sys
+name = sys.argv[0]
+num1 = float(sys.argv[1])
+num2 = float(sys.argv[2])
+#num3 = float(sys.argv[3])
+print(f"The sum of the two numbers: {num1+num2} in {name}")
---
+++ area_utils.py
@@ -0,0 +1,7 @@
+
+def area_circle(radius):
+    return 3.14*radius**2
+def area_rectangle(width,length):
+    return width*length
+def area_triangle(bass,height):
+    return 0.5*bass*height
