e5e9c0b5ee85a4da88766d739ced0001021d8c28
danzi99 <nanjoo@gmail.com>
2026-01-13 18:56:20 +0000

    Update add.py
    block all the actions and add print hello.

--- add.py
+++ add.py
@@ -1,6 +1,7 @@
-import sys
+"""import sys
 name = sys.argv[0]
 num1 = float(sys.argv[1])
 num2 = float(sys.argv[2])
 #num3 = float(sys.argv[3])
-print(f"The sum of the two numbers: {num1+num2} in {name}")
+print(f"The sum of the two numbers: {num1+num2} in {name}")"""
+print("Hello, it's my Github")
